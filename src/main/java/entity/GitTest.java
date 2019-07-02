b6139b8c6ec04c64d03729da88f54936abdce740
dddd <yangoojie@126.com>
2019-07-02 14:33:08 +0800

    第一次更新

--- src/main/java/entity/GitTest.java
+++ src/main/java/entity/GitTest.java
@@ -2,10 +2,11 @@ package entity;
 
 public class GitTest {
     public static void main(String[] args) {
+        int i=0;
         System.out.println("Hello World!");
         int a=1,b=2;
         System.out.println(a+b);
-        for(int i=1;i<=10,i++){
+        for(i=1;i<=10;i++){
         i+=i;
         }
         System.out.println(i);
