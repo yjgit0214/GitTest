001a976e09b034818b1d1c356cb421de445f3507
dddd <yangoojie@126.com>
2019-07-02 14:25:45 +0800

    第一次更新

--- src/main/java/entity/GitTest.java
+++ src/main/java/entity/GitTest.java
@@ -3,5 +3,7 @@ package entity;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a=1,b=2;
+        System.out.println(a+b);
     }
 }
